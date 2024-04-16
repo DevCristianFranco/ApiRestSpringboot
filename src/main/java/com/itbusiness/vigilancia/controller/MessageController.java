f234e363afcd7c8cc6b1ed8587522578ab7d125e
Cristian Franco <cristianfran87@gmail.com>
2024-04-16 12:53:34 -0500

    Documentacion

--- src/main/java/com/itbusiness/vigilancia/controller/MessageController.java
+++ src/main/java/com/itbusiness/vigilancia/controller/MessageController.java
@@ -4,8 +4,8 @@ import com.itbusiness.vigilancia.service.DTO.DeviceMessageId;
 import com.itbusiness.vigilancia.service.DTO.InputDevices;
 import com.itbusiness.vigilancia.service.DTO.OutputMessage;
 import com.itbusiness.vigilancia.service.MessageService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,11 +17,13 @@ public class MessageController {
     private MessageService service;
 
     @PostMapping("message/")
+    @Operation(summary = "Se pueda obtener el mensaje que se emite.")
     public ResponseEntity<OutputMessage> reciveMessages(@RequestBody InputDevices input) {
         return service.ReciveMessages(input);
     }
 
     @PostMapping("message_split/{device_id}")
+    @Operation(summary = "Almacena los diferentes mensajes recibidos para luego armar el resultado")
     public ResponseEntity reciveMessage(
             @PathVariable(required=true,name="device_id") int device_id,
             @RequestBody DeviceMessageId deviceMessageId) {
@@ -29,6 +31,7 @@ public class MessageController {
     }
 
     @GetMapping("message_split/")
+    @Operation(summary = "Responde si fue posible construir el mensaje a partir de los mensajes almacenados")
     public ResponseEntity<OutputMessage> reciveMessage() {
         return service.GetMessage();
     }
